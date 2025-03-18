82ec97ae5b4ec8c1562cbfd2186b0282c801534a
leducanh112 <duca1122001@gmail.com>
2025-03-18 20:14:09 +0700

    Edit

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollToSignUp() {
+  document.getElementById("signup").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToExplore() {
+  document.getElementById("explore").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToProblemSolution() {
+  document
+    .getElementById("problem-solution")
+    .scrollIntoView({ behavior: "smooth" });
+}
