45188f759b692c8666c102173be4607eb60b38d7
leducanh112 <duca1122001@gmail.com>
2025-03-19 20:12:50 +0700

    Edit

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function scrollToProblemSolution() {
     .getElementById("problem-solution")
     .scrollIntoView({ behavior: "smooth" });
 }
+
+function sendData() {
+  var email = document.getElementById("email-input");
+  if (!email.checkValidity()) {
+    return;
+  }
+  var url =
+    "https://script.google.com/macros/s/AKfycbz4a-C8ew0vgfIIWLN8GN8YYFzIe87kHVrVbXMP1L4FVWjZZ2Sit1xZxQ7wtswsdIA/exec";
+  fetch(url, {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: email.value }),
+  }).then(() => {
+    alert("Email submitted successfully!");
+  });
+}
